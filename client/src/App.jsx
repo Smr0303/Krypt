e00b469a1285f18943017c50040dfba0f8d5b33b
Smr0303 <samarmohite7@gmail.com>
2022-06-22 10:40:05 +0530

    Started contracts

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Samss House</h1>
+    </div>
+  );
+}
+export default App;
