74fc6784b645d8dd12fa1ab24ac84014fb8f83cf
Smr0303 <samarmohite7@gmail.com>
2022-06-25 12:24:31 +0530

    Footer done

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -7,6 +7,12 @@ function Transaction() {
 
   const {connectedAccount} = useContext(TransactionContext);
 
+  const TransactionCard =()=>{
+    return(
+      <div></div>
+    )
+  }
+
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
     <div className="flex flex-col md:p-12 py-12 px-4">
