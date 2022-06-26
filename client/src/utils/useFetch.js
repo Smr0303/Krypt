f966901c3e5b7fe8b0ac03667145d1e334bfe911
Smr0303 <samarmohite7@gmail.com>
2022-06-26 11:04:54 +0530

    final fixes done

--- client/src/utils/useFetch.js
+++ client/src/utils/useFetch.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 
-const API_KEY = import.meta.env.VITE_GIPHY_API;
 
 const useFetch = ({ keyword }) => {
   const [gifUrl, setGifUrl] = useState("");
 
   const fetchGifs = async () => {
     try {
-      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
       const { data } = await response.json();
 
       setGifUrl(data[0]?.images?.downsized_medium.url);
